fda5f20afbfdc581db6d5d0b7ccc4eb9458036f9
مِيزَانُ الْدِّينْ عَبْدُ ذِيْ الْجَلَالِ وَ الْإِكْرَامِ <meezaan@giffgaff.co.uk>
2019-09-27 12:21:14 +0100

    Update MySql

--- src/Monitors/MySql.php
+++ src/Monitors/MySql.php
@@ -25,7 +25,7 @@ class MySql extends Monitor
         try {
             $this->db = DriverManager::getConnection($connectionParams, new Configuration());
 
-            $this->status = $this->db->executeQuery($sql);
+            $this->status = (bool) $this->db->fetchAssoc($sql);
         } catch (Exception $e) {
             $this->status = false;
         }
--- tests/Unit/MySqlTest.php
+++ tests/Unit/MySqlTest.php
@@ -14,15 +14,15 @@ class MysqlTest extends TestCase
         $this->assertTrue($monitor->status);
     }
 
-    public function testUnknownServerStatus()
+    public function testUnknownServerOrBadUserStatus()
     {
-        $monitor = new MySql('0.0.0.0', 3315, 'circleci', 'circleci', 'circleci', 'SELECT TIMESTAMP(\'2009-05-18\')');
+        $monitor = new MySql('0.0.0.0', 3306, 'baduser', 'circleci', 'circleci', 'SELECT TIMESTAMP(\'2009-05-18\')');
         $this->assertFalse($monitor->status);
     }
 
     public function testInvalidQuery()
     {
-        $monitor = new MySql('0.0.0.0', 3315, 'circleci', 'circleci', 'circleci', 'SELECTa id from not_exists LIMIT 1');
+        $monitor = new MySql('0.0.0.0', 3306, 'circleci', 'circleci', 'circleci', 'SELECTa id from not_exists LIMIT 1');
         $this->assertFalse($monitor->status);
     }
 
